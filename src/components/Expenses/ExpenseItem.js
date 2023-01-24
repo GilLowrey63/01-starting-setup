2e233c3284cfb64f4a5eec111d5a3ebae739120e
Gil Lowrey <gillowrey@gmail.com>
2023-01-23 21:34:10 -0700

    init

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,21 @@
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
+
+//nice to add the term 'Handler' to the end of functions that are in charge of events
+//this way I know the click function is an eventHANDLER (see what I did there)
+//and I am not calling it elsewhere
